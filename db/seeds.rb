1977ce935d93aa655b935f57549739d82cb66b21
Anna Nonaka <annanonaka@gmail.com>
2021-07-08 19:17:40 +0900

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+puts "Generate 5 restaurants..."
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+end
+puts "Generated!"
+
+# Review.destroy_all
+
+# puts "Generate reviews"
+# 5.times do
+#   Review.create(
+#     rating: rand(0..5),
+#     content: Faker::Restaurant.review
+#     # restaurant_id: 1
+#     )
+# end
+# puts "Generated!"
